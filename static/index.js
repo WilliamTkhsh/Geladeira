36e3e121917ce37ab627d6a3f932a2bd2184da35
William Takahashi <william@levetec.com.br>
2022-09-30 16:49:09 -0300

    melhorando forms para preencher cep

--- static/index.js
+++ static/index.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     $('.button_choice').click(function(e){
+        $('.formCep').attr('style', 'display:block');
+        window.scrollTo(0,2000);
+    });
+    $('.button_send').click(function(e){
         if($("input[name=cep]").val() == ''){
-            alert("Preencha o campo de CEP")
-        } else {
-            $("enviado").css('display', 'block');
-            window.scrollTo(0,2000);
+            alert('Preencha o campo de CEP');
         }
     });
 })
